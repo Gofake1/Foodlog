35d6d577880cdec8ab2982813da3cecf5fb3236f
David Wu <david@gofake1.net>
2017-12-12 15:56:42 -0800

    Initial Commit

---
+++ Foodlog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodlog
+//
+//  Created by David on 12/12/17.
+//  Copyright © 2017 Gofake1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
