659228577d75e4764ab49b2e6f376cd801dbd9a4
David Wu <david@gofake1.net>
2018-03-18 18:13:47 -0700

    Show nutrition information for Food in LogDetailVC

--- Foodlog/LogDetailViewController.swift
+++ Foodlog/LogDetailViewController.swift
@@ -66,6 +66,61 @@ protocol LogDetailPresentable {
     func makeDetailText(_ representation: NutritionKind.ValueRepresentation) -> String
 }
 
+struct NutritionPrinter {
+    var print: String {
+        var str = ""
+        str += makeLine(.calories, food.calories)
+        str += makeLine(.totalFat, food.totalFat)
+        str += makeLine(.saturatedFat, food.saturatedFat)
+        str += makeLine(.monounsaturatedFat, food.monounsaturatedFat)
+        str += makeLine(.polyunsaturatedFat, food.polyunsaturatedFat)
+        str += makeLine(.transFat, food.transFat)
+        str += makeLine(.cholesterol, food.cholesterol)
+        str += makeLine(.sodium, food.sodium)
+        str += makeLine(.totalCarbohydrate, food.totalCarbohydrate)
+        str += makeLine(.dietaryFiber, food.dietaryFiber)
+        str += makeLine(.sugars, food.sugars)
+        str += makeLine(.protein, food.protein)
+        str += makeLine(.vitaminA, food.vitaminA)
+        str += makeLine(.vitaminB6, food.vitaminB6)
+        str += makeLine(.vitaminB12, food.vitaminB12)
+        str += makeLine(.vitaminC, food.vitaminC)
+        str += makeLine(.vitaminD, food.vitaminD)
+        str += makeLine(.vitaminE, food.vitaminE)
+        str += makeLine(.vitaminK, food.vitaminK)
+        str += makeLine(.calcium, food.calcium)
+        str += makeLine(.iron, food.iron)
+        str += makeLine(.magnesium, food.magnesium)
+        str += makeLine(.potassium, food.potassium)
+        if str == "" {
+            str = "No Information\n"
+        }
+        return str
+    }
+    private let food: Food
+    private let makeLine: (NutritionKind, Float) -> String
+    
+    init(_ food: Food, _ representation: NutritionKind.ValueRepresentation, _ transform: @escaping (Float) -> Float) {
+        self.food = food
+        switch representation {
+        case .percentage:
+            makeLine = { kind, real in
+                guard real != 0.0 else { return "" }
+                if let percentage = real.dailyValuePercentageFromReal(kind) {
+                    return kind.description+"\t"+transform(percentage).pretty!+"%\n"
+                } else {
+                    return kind.description+"\t"+transform(real).pretty!+kind.unit.suffix+"\n"
+                }
+            }
+        case .real:
+            makeLine = { kind, real in
+                guard real != 0.0 else { return "" }
+                return kind.description+"\t"+transform(real).pretty!+kind.unit.suffix+"\n"
+            }
+        }
+    }
+}
+
 extension Food: LogDetailPresentable {
     var logDetailTitle: String {
         return name
@@ -75,12 +130,11 @@ extension Food: LogDetailPresentable {
     }
     
     func editDetailPresentable() {
-        // TODO
+        VCController.editFood(self)
     }
     
     func makeDetailText(_ representation: NutritionKind.ValueRepresentation) -> String {
-        // TODO
-        return ""
+        return "Per \(measurementRepresentation.singular):\n" + NutritionPrinter(self, representation, { $0 }).print
     }
 }
 
@@ -98,50 +152,7 @@ extension FoodEntry: LogDetailPresentable {
     }
     
     func makeDetailText(_ representation: NutritionKind.ValueRepresentation) -> String {
-        func detailString(_ real: Float, _ kind: NutritionKind) -> String {
-            guard real != 0.0 else { return "" }
-            let totalValue = real * measurementFloat
-            switch representation {
-            case .percentage:
-                if let percentage = totalValue.dailyValuePercentageFromReal(kind)?.pretty {
-                    return "\(kind)\t\(percentage)%\n"
-                } else {
-                    return "\(kind)\t\(totalValue.pretty!)\(kind.unit.suffix)\n"
-                }
-            case .real:
-                return "\(kind)\t\(totalValue.pretty!)\(kind.unit.suffix)\n"
-            }
-        }
-        
-        guard let food = food else { return "Error: Food Information\n" }
-        var str = ""
-        str += detailString(food.calories, .calories)
-        str += detailString(food.totalFat, .totalFat)
-        str += detailString(food.saturatedFat, .saturatedFat)
-        str += detailString(food.monounsaturatedFat, .monounsaturatedFat)
-        str += detailString(food.polyunsaturatedFat, .polyunsaturatedFat)
-        str += detailString(food.transFat, .transFat)
-        str += detailString(food.cholesterol, .cholesterol)
-        str += detailString(food.sodium, .sodium)
-        str += detailString(food.totalCarbohydrate, .totalCarbohydrate)
-        str += detailString(food.dietaryFiber, .dietaryFiber)
-        str += detailString(food.sugars, .sugars)
-        str += detailString(food.protein, .protein)
-        str += detailString(food.vitaminA, .vitaminA)
-        str += detailString(food.vitaminB6, .vitaminB6)
-        str += detailString(food.vitaminB12, .vitaminB12)
-        str += detailString(food.vitaminC, .vitaminC)
-        str += detailString(food.vitaminD, .vitaminD)
-        str += detailString(food.vitaminE, .vitaminE)
-        str += detailString(food.vitaminK, .vitaminK)
-        str += detailString(food.calcium, .calcium)
-        str += detailString(food.iron, .iron)
-        str += detailString(food.magnesium, .magnesium)
-        str += detailString(food.potassium, .potassium)
-        if str == "" {
-            str = "No Information\n"
-        }
-        return str
+        return NutritionPrinter(food!, representation, { [measurementFloat] in $0 * measurementFloat }).print
     }
 }
 
@@ -151,9 +162,9 @@ extension Food.MeasurementRepresentation {
         case .serving:      return "×"
         case .milligram:    return " mg"
         case .gram:         return " g"
-        case .ounce:        return " oz."
-        case .pound:        return " lb."
-        case .fluidOunce:   return " oz."
+        case .ounce:        return " oz"
+        case .pound:        return " lb"
+        case .fluidOunce:   return " oz"
         }
     }
 }
